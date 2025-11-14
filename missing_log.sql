263b1d41b28815bddee541aaa8cf857eb11b28f9
Shoaib <75385174+Shoaib4027@users.noreply.github.com>
2025-11-14 17:46:53 +0530

    Document missing sequence files in missing_log.sql
    Added a comment to indicate missing sequence files for MIQ4.

--- missing_log.sql
+++ missing_log.sql
@@ -16,3 +16,8 @@ WHERE
     AND thread# = 1
 ORDER BY 
     sequence#;
+
+
+
+miib_arch_2_161117_1156944397.log miib_arch_5_134327_1156944397.log miib_arch_2_161118_1156944397.log miib_arch_6_125329_1156944397.log miib_arch_6_125328_1156944397.log miib_arch_2_161116_1156944397.log 
+this are file i want to find missing sequence suppose between 161116 and 16118 for miib_arch_2 how will i do it linux
