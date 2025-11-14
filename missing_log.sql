4a1eef2eba75b0d4c5f919977043c9596a8b2194
Shoaib <75385174+Shoaib4027@users.noreply.github.com>
2025-11-14 17:49:19 +0530

    Update missing_log.sql

--- missing_log.sql
+++ missing_log.sql
@@ -1,23 +1,24 @@
-SELECT 
-    consumer_name,
-    name,
-    thread#,
-    sequence#,
-    (sequence# - LAG(sequence#) OVER (PARTITION BY thread# ORDER BY sequence#)) AS diff,
-    CASE 
-        WHEN (sequence# - LAG(sequence#) OVER (PARTITION BY thread# ORDER BY sequence#)) > 1 
-        THEN 'MISSING: ' || (LAG(sequence#) OVER (PARTITION BY thread# ORDER BY sequence#) + 1) 
-             || ' TO ' || (sequence# - 1)
-    END AS missing_range
-FROM 
-    dba_registered_archived_log
-WHERE 
-    consumer_name = 'MIQ4'
-    AND thread# = 1
-ORDER BY 
-    sequence#;
+#!/bin/bash
 
+ls miib_arch_* | \
+awk -F'[_ .]' '
+{
+    thread=$3; 
+    seq=$4;
+    key=thread;
+    arr[key][seq]=1;
 
+    if (seq > max[key]) max[key]=seq;
+    if (min[key] == "" || seq < min[key]) min[key]=seq;
+}
 
-miib_arch_2_161117_1156944397.log miib_arch_5_134327_1156944397.log miib_arch_2_161118_1156944397.log miib_arch_6_125329_1156944397.log miib_arch_6_125328_1156944397.log miib_arch_2_161116_1156944397.log 
-this are file i want to find missing sequence suppose between 161116 and 16118 for miib_arch_2 how will i do it linux
+END {
+    for (t in arr) {
+        printf "Thread %s:\n", t;
+        for (i=min[t]; i<=max[t]; i++) {
+            if (!(i in arr[t])) {
+                printf "  Missing sequence: %d\n", i;
+            }
+        }
+    }
+}'
