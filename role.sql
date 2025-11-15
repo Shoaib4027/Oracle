3dffd3c2fba76614686fc4e820fa064178d2f625
Shoaib <75385174+Shoaib4027@users.noreply.github.com>
2025-11-15 15:23:16 +0530

    Update role.sql

--- role.sql
+++ role.sql
@@ -1,13 +1,21 @@
 SELECT 'SYS PRIV' AS type,
-       grantee,
-       privilege
-FROM dba_sys_privs
+       sp.grantee,
+       sp.privilege
+FROM dba_sys_privs sp
+WHERE sp.grantee IN (SELECT username FROM dba_users WHERE oracle_maintained = 'N')
+
 UNION ALL
-SELECT 'ROLE', grantee, granted_role
-FROM dba_role_privs
+SELECT 'ROLE',
+       rp.grantee,
+       rp.granted_role
+FROM dba_role_privs rp
+WHERE rp.grantee IN (SELECT username FROM dba_users WHERE oracle_maintained = 'N')
+
 UNION ALL
 SELECT 'OBJ PRIV',
-       grantee,
-       owner || '.' || table_name || ' - ' || privilege
-FROM dba_tab_privs
-ORDER BY grantee;
+       tp.grantee,
+       tp.owner || '.' || tp.table_name || ' - ' || tp.privilege
+FROM dba_tab_privs tp
+WHERE tp.grantee IN (SELECT username FROM dba_users WHERE oracle_maintained = 'N')
+
+ORDER BY grantee, type;
