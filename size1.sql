40cf1737cfdf71588c67018a4e39a0ff8c7f9b6b
Shoaib <75385174+Shoaib4027@users.noreply.github.com>
2026-09-25 11:02:54 +0530

    Update size1.sql

--- size1.sql
+++ size1.sql
@@ -1,34 +1,36 @@
-COL TABLE_OWNER        FOR A30
-COL TABLE_NAME         FOR A50
-COL PARTITION_NAME     FOR A30
-COL SUBPARTITION_NAME  FOR A30
+COL TABLE_NAME        FOR A50
+COL TABLE_OWNER       FOR A30
+COL PARTITION_NAME    FOR A30
+COL SUBPARTITION_NAME FOR A30
 
-WITH owner_ AS
+WITH
+owner_
+AS
 (
     SELECT 'EISAPP' owner
     FROM dual
 ),
 
 /* ============================================================
-   HASH SUBPARTITION SIZE
+   TABLE SUBPARTITION SIZE
    ============================================================ */
-subpart_size AS
+ps
+AS
 (
     SELECT
         tsp.table_owner,
         tsp.table_name,
         tsp.partition_name,
         tsp.subpartition_name,
-        SUM(NVL(sg.bytes,0)) / 1024 / 1024 AS subpart_size_mb
-    FROM dba_tab_subpartitions tsp
-         LEFT JOIN dba_segments sg
-           ON sg.owner = tsp.table_owner
-          AND sg.segment_name = tsp.table_name
-          AND sg.partition_name = tsp.partition_name
-          AND sg.subpartition_name = tsp.subpartition_name
-    WHERE tsp.table_owner = (SELECT owner FROM owner_)
-      AND tsp.table_name = '&tab_name'
-      AND tsp.partition_name = '&part_name'
+        SUM(sg.bytes)/(1024*1024) part_size_mb
+    FROM dba_tab_subpartitions tsp,
+         dba_segments sg,
+         owner_ o
+    WHERE tsp.table_owner       = o.owner
+      AND sg.owner              = tsp.table_owner
+      AND sg.segment_name       = tsp.table_name
+      AND sg.partition_name     = tsp.partition_name
+      AND sg.subpartition_name  = tsp.subpartition_name
     GROUP BY
         tsp.table_owner,
         tsp.table_name,
@@ -39,28 +41,28 @@ subpart_size AS
 /* ============================================================
    LOB SUBPARTITION SIZE
    ============================================================ */
-lob_size AS
+pls
+AS
 (
     SELECT
         tsp.table_owner,
         tsp.table_name,
         tsp.partition_name,
         tsp.subpartition_name,
-        SUM(NVL(sg.bytes,0)) / 1024 / 1024 AS lob_size_mb
-    FROM dba_tab_subpartitions tsp
-         JOIN dba_lob_partitions lp
-           ON lp.table_owner = tsp.table_owner
-          AND lp.table_name = tsp.table_name
-          AND lp.partition_name = tsp.partition_name
-          AND lp.subpartition_name = tsp.subpartition_name
-         JOIN dba_segments sg
-           ON sg.owner = lp.table_owner
-          AND sg.segment_name = lp.lob_name
-          AND sg.partition_name = lp.partition_name
-          AND sg.subpartition_name = lp.subpartition_name
-    WHERE tsp.table_owner = (SELECT owner FROM owner_)
-      AND tsp.table_name = '&tab_name'
-      AND tsp.partition_name = '&part_name'
+        SUM(sg.bytes)/(1024*1024) part_lob_size_mb
+    FROM dba_tab_subpartitions tsp,
+         dba_lob_subpartitions lsp,
+         dba_segments sg,
+         owner_ ow
+    WHERE tsp.table_owner       = ow.owner
+      AND lsp.table_owner       = tsp.table_owner
+      AND lsp.table_name        = tsp.table_name
+      AND lsp.parent_table_partition = tsp.partition_name
+      AND lsp.parent_table_subpartition = tsp.subpartition_name
+      AND sg.owner              = lsp.table_owner
+      AND sg.segment_name       = lsp.lob_name
+      AND sg.partition_name     = lsp.partition_name
+      AND sg.subpartition_name  = lsp.subpartition_name
     GROUP BY
         tsp.table_owner,
         tsp.table_name,
@@ -69,23 +71,24 @@ lob_size AS
 )
 
 SELECT
-    s.table_owner,
-    s.table_name,
-    s.partition_name,
-    s.subpartition_name,
-    ROUND(s.subpart_size_mb,2) AS subpart_size_mb,
-    ROUND(NVL(l.lob_size_mb,0),2) AS lob_size_mb,
+    ps.table_owner,
+    ps.table_name,
+    ps.partition_name,
+    ps.subpartition_name,
+    ROUND(ps.part_size_mb,2) part_size_mb,
+    ROUND(NVL(pls.part_lob_size_mb,0),2) part_lob_size_mb,
     ROUND(
-        s.subpart_size_mb +
-        NVL(l.lob_size_mb,0),
+        ps.part_size_mb + NVL(pls.part_lob_size_mb,0),
         2
-    ) AS total_size_mb
-FROM subpart_size s
-LEFT JOIN lob_size l
-       ON l.table_owner       = s.table_owner
-      AND l.table_name        = s.table_name
-      AND l.partition_name    = s.partition_name
-      AND l.subpartition_name = s.subpartition_name
+    ) all_part_size_mb
+FROM ps,
+     pls
+WHERE pls.table_owner(+)       = ps.table_owner
+  AND pls.table_name(+)        = ps.table_name
+  AND pls.partition_name(+)    = ps.partition_name
+  AND pls.subpartition_name(+) = ps.subpartition_name
+  AND ps.table_name            = '&tab_name'
+  AND ps.partition_name        = '&part_name'
 ORDER BY
-    s.partition_name,
-    s.subpartition_name;
+    ps.partition_name,
+    ps.subpartition_name;
